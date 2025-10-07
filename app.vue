d33aae3bb919eb2147c4f5ffb276a704c39de0e9
kamilwozniak <wozniak.software@gmail.com>
2025-10-07 21:20:52 +0200

    feat: add TestComponent and integrate into app.vue; create test API endpoint

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <TestComponent />
   </div>
 </template>
+
+<script setup lang="ts">
+  import TestComponent from './components/TestComponent.vue'
+</script>
---
+++ components/TestComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="test-component">
+    <h1>Hello World</h1>
+  </div>
+</template>
+<script setup lang="ts">
+  onMounted(() => {
+    console.log('TestComponent mounted')
+  })
+</script>
+<style scoped>
+.test-component {
+  color: red;
+}
+</style>
