9723b848627feded09d419d69d6b5068066ced1b
kamilwozniak <wozniak.software@gmail.com>
2025-10-09 18:56:37 +0200

    feat: add Prettier configuration and ignore files, update ESLint settings to integrate Prettier, and adjust package dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,21 +2,37 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 import vueParser from 'vue-eslint-parser'
 import tsParser from '@typescript-eslint/parser'
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
 
-export default withNuxt().append(
-  {
-    files: ['**/*.vue'],
-    languageOptions: {
-      parser: vueParser,
-      parserOptions: {
-        parser: tsParser,
+export default withNuxt()
+  .append(prettierConfig)
+  .append(
+    {
+      files: ['**/*.vue'],
+      languageOptions: {
+        parser: vueParser,
+        parserOptions: {
+          parser: tsParser,
+        },
+      },
+      plugins: {
+        prettier: prettierPlugin,
+      },
+      rules: {
+        'prettier/prettier': 'warn',
       },
     },
-  },
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    languageOptions: {
-      parser: tsParser,
-    },
-  }
-)
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      languageOptions: {
+        parser: tsParser,
+      },
+      plugins: {
+        prettier: prettierPlugin,
+      },
+      rules: {
+        'prettier/prettier': 'warn',
+      },
+    }
+  )
